605f6c42dbf2b6e7cabf0a596d3286073c5c3c55
Alex <78284921+AleexAg@users.noreply.github.com>
2026-02-12 09:14:26 -0300

    Create juegos.js

---
+++ juegos.js
@@ -0,0 +1,66 @@
+[
+  {
+    "id": 1,
+    "nombre": "Elden Ring",
+    "precio": 60000,
+    "categoria": "RPG",
+    "edad": "+16",
+    "descripcion": "Explora un vasto mundo abierto lleno de desafíos y criaturas oscuras."
+  },
+  {
+    "id": 2,
+    "nombre": "God of War Ragnarök",
+    "precio": 70000,
+    "categoria": "Aventura",
+    "edad": "+18",
+    "descripcion": "Kratos y Atreus enfrentan el destino en una épica aventura nórdica."
+  },
+  {
+    "id": 3,
+    "nombre": "FIFA 26",
+    "precio": 50000,
+    "categoria": "Deportes",
+    "edad": "ATP",
+    "descripcion": "La experiencia futbolística más realista con equipos actualizados."
+  },
+  {
+    "id": 4,
+    "nombre": "GTA VI",
+    "precio": 80000,
+    "categoria": "Acción",
+    "edad": "+18",
+    "descripcion": "Un nuevo mundo criminal con gráficos de última generación."
+  },
+  {
+    "id": 5,
+    "nombre": "The Last of Us Part III",
+    "precio": 75000,
+    "categoria": "Acción / Aventura",
+    "edad": "+18",
+    "descripcion": "Una historia intensa de supervivencia y emociones extremas."
+  },
+  {
+    "id": 6,
+    "nombre": "Cyberpunk 2077: Ultimate Edition",
+    "precio": 65000,
+    "categoria": "RPG",
+    "edad": "+18",
+    "descripcion": "Explora Night City con mejoras y expansión incluida."
+  },
+  {
+    "id": 7,
+    "nombre": "Minecraft Deluxe",
+    "precio": 30000,
+    "categoria": "Sandbox",
+    "edad": "ATP",
+    "descripcion": "Construye, explora y sobrevive en mundos infinitos."
+  },
+  {
+    "id": 8,
+    "nombre": "Call of Duty Modern Warfare IV",
+    "precio": 72000,
+    "categoria": "Shooter",
+    "edad": "+18",
+    "descripcion": "Acción militar intensa con multijugador competitivo."
+  }
+]
